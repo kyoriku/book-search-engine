1139eadf5aad3100e540538515e7e1500324b31b
Austin Graham <austiingraham@gmail.com>
2024-03-20 16:24:01 -0400

    added comments to SavedBooks.jsx and SearchBooks.jsx

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -4,41 +4,53 @@ import {
   Button,
   Row,
   Col
-} from 'react-bootstrap';
+} from 'react-bootstrap'; // Importing the necessary components from react-bootstrap
 
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-import { useQuery, useMutation } from '@apollo/client';
+import Auth from '../utils/auth'; // Importing the Auth module from the utils folder
+import { removeBookId } from '../utils/localStorage'; // Importing the removeBookId function from the localStorage module
+import { GET_ME } from '../utils/queries'; // Importing the GET_ME query from the queries module
+import { REMOVE_BOOK } from '../utils/mutations'; // Importing the REMOVE_BOOK mutation from the mutations module
+import { useQuery, useMutation } from '@apollo/client'; // Importing the necessary hooks from the apollo/client module
 
+// Declaring the SavedBooks functional component, which takes in no parameters
 const SavedBooks = () => {
+  // Destructuring the loading and data properties from the useQuery hook, setting the GET_ME query as the query property
   const { loading, data } = useQuery(GET_ME);
+  // Destructuring the me property from the data object, setting it as the userData variable
   const userData = data?.me || {};
+  // Destructuring the removeBook mutation from the useMutation hook
   const [removeBook] = useMutation(REMOVE_BOOK);
 
+  // Declaring the handleDeleteBook function, which takes in a bookId parameter
   const handleDeleteBook = async (bookId) => {
+    // Declaring the token variable and setting it to the result of the loggedIn method from the Auth module
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
+    // If the token does not exist, return false and exit the function
     if (!token) {
       return false;
     }
 
+    // Try block to execute the removeBook mutation
     try {
       const { data } = await removeBook({
         variables: { bookId }
       });
       
+      // Executing the removeBookId function with the bookId parameter
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
+  // If the loading property is true, return a heading element with the text 'LOADING...'
   if (loading) {
     return <h2>LOADING...</h2>;
   }
 
+  // If the userData.savedBooks array has a length of 0, return a heading element with the text 'You have no saved books!'
+  // Otherwise, return a heading element with the text 'Viewing saved books!' and display each book in the userData.savedBooks array 
   return (
     <>
       <div className="text-light bg-dark p-5">
@@ -77,4 +89,4 @@ const SavedBooks = () => {
   );
 };
 
-export default SavedBooks;
+export default SavedBooks; // Exporting the SavedBooks component
